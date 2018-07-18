0b773a9a4ba967d4d86882df6ed90d8129299c34
Deepak kumar <31114647+deepak1928@users.noreply.github.com>
2018-07-19 02:12:26 +0530

    Update Iris_Data.py

--- Iris_Data.py
+++ Iris_Data.py
@@ -1,17 +1,15 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
 data=pd.read_csv('Iris_Data.csv')
-
- 
-
 X=data.iloc[:,0:4].values
 y=data.iloc[:,4:5].values
  
-from sklearn.preprocessing import LabelEncoder
-labelencoder_Y=LabelEncoder()
-y[:,0]=labelencoder_Y.fit_transform(y[:,0])
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+labelencoder_y=LabelEncoder()
+y[:,0]=labelencoder_y.fit_transform(y[:,0])
+onehotencoder=OneHotEncoder(categorical_features=[0])
+y=onehotencoder.fit_transform(y).toarray() 
 #Test data
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
@@ -39,7 +37,7 @@ classifier.add(Dense(  6, kernel_initializer = 'uniform', activation = 'relu', i
 classifier.add(Dense(  6,kernel_initializer = 'uniform', activation = 'relu'))
 
 # Adding the output layer
-classifier.add(Dense( 1,kernel_initializer = 'uniform', activation = 'sigmoid'))
+classifier.add(Dense( 3,kernel_initializer = 'uniform', activation = 'sigmoid'))
 
 # Compiling the ANN
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
@@ -48,11 +46,27 @@ classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = [
 classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
 
 # Part 3 - Making the predictions and evaluating the model
-
-# Predicting the Test set results
 y_pred = classifier.predict(X_test)
-y_pred = (y_pred > 0.5)
-
+y_new=np.zeros(30) 
+for i in range(0,30):
+        k=0
+        for j in range(1,3):
+            pivot=y_pred[i][k]
+            if y_pred[i][j] > pivot:
+                pivot=y_pred[i][j]
+                k=j
+        y_new[i]=k
+            
+y_pred1=np.zeros(30) 
+for i in range(0,30):
+        k=0
+        for j in range(1,3):
+            pivot=y_test[i][k]
+            if y_test[i][j] > pivot:
+                pivot=y_test[i][j]
+                k=j
+        y_pred1[i]=k
+            
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
+cm = confusion_matrix(y_pred1, y_new)
